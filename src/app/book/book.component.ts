deac73f59ba8ef35dad02bc8fc5f329f9e6bfb2d
jstran9 <wptran58@gmail.com>
2019-06-01 11:08:15 -0700

    specifying how to call the book component, what view to use, and the location of the custom stylesheet.

---
+++ src/app/book/book.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+import { Book } from './book';
+
+@Component({
+  selector: 'app-book',
+  templateUrl: './book.component.html',
+  styleUrls: ['./book.component.css', ],
+})
+export class BookComponent {
+
+}
