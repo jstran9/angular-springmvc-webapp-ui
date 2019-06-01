628a6803b7f4a3b8659222226479cdcc7d137891
jstran9 <wptran58@gmail.com>
2019-06-01 11:32:17 -0700

    creating http get request to retrieve all the books.

---
+++ src/app/book/book.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpResponse, HttpHeaderResponse, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Book } from './book';
+import { catchError, map } from 'rxjs/operators';
+
+
+@Injectable()
+export class BookService {
+  private API_BASE_URL = `http://localhost:8080/api/`;
+  private GET_ALL_BOOKS = `${this.API_BASE_URL}books`;
+
+  /*
+   * dependency injection used here
+   * let Angular's dependency injector inject the necessary dependency.
+   */
+  constructor(private httpClient: HttpClient) {}
+
+  getAllBooks(): Observable<Book[]> {
+    return this.httpClient.get<Book[]>(this.GET_ALL_BOOKS).pipe(
+      map(response => response),
+      catchError(this.handleError)
+    );
+  }
+
+  private handleError(error: HttpResponse<any>) {
+    return Observable.throw(error);
+  }
+}
