5da180d16d810ce61db5b964ac0a4538adebb676
jstran9 <wptran58@gmail.com>
2019-06-01 11:32:38 -0700

    adding necessary configuration to the main module to be able to use the BookService.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {BookComponent} from './book/book.component';
+import { BookService } from './book/book.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -10,9 +12,12 @@ import {BookComponent} from './book/book.component';
     BookComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, HttpClientModule
   ],
-  providers: [],
+  /*
+   * Tell Angular that our provider is BookService.
+   */
+  providers: [ BookService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
