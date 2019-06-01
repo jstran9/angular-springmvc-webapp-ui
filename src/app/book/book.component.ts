45a6f783e0699906ebbcb5b3f51e753e5d02ed13
jstran9 <wptran58@gmail.com>
2019-06-01 11:45:00 -0700

    invoking a bookservice call to retrieve the books and to output the result to the console.

--- src/app/book/book.component.ts
+++ src/app/book/book.component.ts
@@ -1,11 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { Book } from './book';
+import { BookService } from './book.service';
 
 @Component({
   selector: 'app-book',
   templateUrl: './book.component.html',
   styleUrls: ['./book.component.css', ],
 })
-export class BookComponent {
+export class BookComponent implements OnInit {
 
+  books: Book[];
+  constructor(private bookService: BookService) {}
+
+  ngOnInit(): void {
+    this.getBooks();
+  }
+
+  getBooks(): void {
+    /*
+     * assign the books array to the returned bookData below.
+     * also log the error response to the console if there is one.
+     */
+    this.bookService.getAllBooks()
+      .subscribe((bookData) => {
+          this.books = bookData,
+            console.log(bookData);
+      }, (error) => {
+        console.log(error);
+      });
+  }
 }
